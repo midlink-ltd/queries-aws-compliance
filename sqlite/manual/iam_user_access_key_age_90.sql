6989b4b1bec0ad93d07cf91d6acc844535d4c015
Yuval Rimar <yuval@blinkops.com>
2021-07-22 16:08:05 +0300

    manually translate iam_user_access_key_age_90

--- sqlite/manual/iam_user_access_key_age_90.sql
+++ sqlite/manual/iam_user_access_key_age_90.sql
@@ -2,11 +2,11 @@ select
   -- Required Columns
   'arn:' || partition || ':iam::' || account_id || ':user/' || user_name || '/accesskey/' || access_key_id as resource,
   case
-    when create_date <= (current_date - interval '90' day) then 'alarm'
+    when create_date <= datetime('now', '-90 day') then 'alarm'
     else 'ok'
   end status,
-  user_name || ' ' || access_key_id || ' created ' || to_char(create_date , 'DD-Mon-YYYY') ||
-    ' (' || extract(day from current_timestamp - create_date) || ' days).'
+  user_name || ' ' || access_key_id || ' created ' || date(create_date) ||
+    ' (' || ROUND(JULIANDAY('now') - JULIANDAY(create_date)) || ' days).'
   as reason,
   -- Additional Dimensions
   account_id
