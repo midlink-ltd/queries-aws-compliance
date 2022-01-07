ef3bf876325ef8c4f8e0cb17ea4cf7482d463691
Jonathan ROQUELAURE <jonathan.roquelaure@gmail.com>
2022-01-07 18:42:30 +0100

    POC on benchmark to playbook

---
+++ playbook_builder/generator.py
@@ -0,0 +1,142 @@
+from genericpath import exists
+import os
+import yaml
+import json
+from dotmap import DotMap
+from yaml import load,dump, loader
+
+#based on https://stackoverflow.com/questions/6432605/any-yaml-libraries-in-python-that-support-dumping-of-long-strings-as-block-liter
+class folded_unicode(str): pass
+class literal_unicode(str): pass
+
+def folded_unicode_representer(dumper, data):
+    return dumper.represent_scalar(u'tag:yaml.org,2002:str', data, style='>')
+def literal_unicode_representer(dumper, data):
+    return dumper.represent_scalar(u'tag:yaml.org,2002:str', data, style='|')
+
+def loadTemplate():
+    with open(os.path.dirname(__file__) + "/playbook_template.yaml") as stream:
+        try:
+            data_map = load(stream, Loader=yaml.FullLoader)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    return data_map
+
+def writePlaybook(data_map, name):
+    with open(os.path.dirname(__file__) + "/out/"+name, "w") as stream:
+        try:
+            noalias_dumper = yaml.dumper.Dumper
+            noalias_dumper.ignore_aliases = lambda self, data: True
+            yaml.dump(data_map,stream, Dumper=noalias_dumper)
+        except yaml.YAMLError as exc:
+            print(exc)
+    return
+
+def findControlFile(query, controlDir):
+    for fname in os.listdir(controlDir):    # change directory as needed
+        if os.path.isfile(fname):    # make sure it's a file, not a directory entry
+            with open(fname) as f:   # open file
+                for line in f:       # process line by line
+                    if query in line:    # search for string
+                        print(fname)
+                        break
+                        
+    return fname
+
+def parseBenchmark(controlFile):
+    benchmark = {}
+    benchmark["controls"] = []
+    benchmark["children"] = []
+
+    with open(os.path.dirname(__file__) + controlFile) as stream:
+        lines = stream.readlines()
+        control = {}
+        i = 0
+        while i < len(lines):
+            if(lines[i].startswith("benchmark")):
+                benchmark["name"] = lines[i].split(' ')[1]
+                while not "children = [" in lines[i]:
+                    i+=1
+                i+=1
+                while not "]" in lines[i]:
+                    benchmark["children"].append(lines[i].strip().replace(',',''))
+                    i+=1
+                while not "}" in lines[i]:
+                    i+=1
+            if(lines[i].startswith("control")):
+                controlName = lines[i].split(' ')[1].replace('"','')
+                child = benchmark["children"].index("control."+controlName)
+                i+=1
+                while not lines[i].startswith("}"):
+                    sLine = lines[i].split("=")
+                    if len(sLine) > 1 :
+                        control[sLine[0].strip()] = sLine[1].strip()
+                    i+=1
+                benchmark["controls"].append(control)
+            i+=1
+   
+    return benchmark 
+            
+def getQuery(queryFile):
+
+    with open(queryFile) as stream:
+       # query = yaml.load(stream,Loader=yaml.FullLoader)
+        lines = stream.read()
+
+    return lines
+
+def convertBenchToPlaybook(bench, playbook):
+    playbook["name"] = bench["name"]
+    with open(os.path.dirname(__file__) + "/section-template.yaml") as stream:
+        stepsTemplate = yaml.load(stream,Loader=yaml.FullLoader)
+    sectionId = 0
+    for control in bench["controls"]:
+        section = DotMap(stepsTemplate["steps"][0])
+        sql = DotMap(stepsTemplate["steps"][1])
+        notif = DotMap(stepsTemplate["steps"][2])
+
+        queryFile = "../sqlite/auto/" + control["sql"].replace("query.",'').replace('"','')
+        
+        if exists(queryFile):
+            # Fill section
+            section.text = "#" + control["title"].replace('"','')
+            # Fill sql query action
+            sql.inputs.sql = " \n " + getQuery(queryFile)
+            sql.id = "S"+str(sectionId+1)
+            #fill notification
+            notif.id = "S"+str(sectionId+2)
+            notif.inputs.Content = notif.inputs.Content.replace("S1", sql.id)
+        else:
+            # Fill section
+            section.text = "#" + control["title"].replace('"','') + " - Not implemented"
+            # Fill sql query action
+            sql.inputs.sql = ""
+            sql.id = "S"+str(sectionId+1)
+            #fill notification
+            notif.id = "S"+str(sectionId+2)
+            notif.inputs.Content = notif.inputs.Content.replace("S1", sql.id)
+        
+        playbook["steps"].append(section.toDict())
+        playbook["steps"].append(sql.toDict())
+        playbook["steps"].append(notif.toDict())
+        sectionId += 2
+    
+    return playbook
+
+
+def main():
+
+   #controlFile = findControlFile("query.apigateway_rest_api_stage_use_ssl_certificate", "/Users/jon/workspace/blink/queries-aws-compliance/foundational_security")
+    cwd = "/../foundational_security/"
+    for file in os.listdir(os.path.dirname(__file__) + cwd):
+        if file.endswith(".sp"):
+            benchmark = parseBenchmark(cwd + file)
+            data_map = loadTemplate()
+    
+            playbook = convertBenchToPlaybook(benchmark, data_map)
+            writePlaybook(playbook, benchmark["name"].replace('"','') + ".yaml")
+
+    
+if __name__ == "__main__":
+    main()
